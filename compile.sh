b320ebc2327dcdcd0d55d4489efd3b51b907d33d
mmorrison <mmorrison@pon4.net>
2018-08-07 23:44:54 -0500

    Move around scripting dir

--- compile.sh
+++ compile.sh
@@ -1,2 +1,8 @@
 #!/bin/bash
-./spcomp -i csgo/addons/sourcemod/scripting/include -i csgo/addons/sourcemod/scripting/ckSurf -i ../scripting/include -o csgo/addons/sourcemod/plugins/ckSurf.smx csgo/addons/sourcemod/scripting/ckSurf.sp
+
+./scripting/spcomp \
+  -i csgo/addons/sourcemod/scripting/include \
+  -i csgo/addons/sourcemod/scripting \
+  -i ../scripting/include \
+  -o csgo/addons/sourcemod/plugins/ckSurf.smx \
+  csgo/addons/sourcemod/scripting/ckSurf.sp
